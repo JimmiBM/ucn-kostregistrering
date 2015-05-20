1282184fc54103dd2b8eb55cdaac631fe5a272fa
Henrik Jacobsen <jacobsen_henrik@hotmail.com>
2015-05-20 13:26:46 +0200

    lavet flere test i userTest.js

--- www/views/user/userTest.js
+++ www/views/user/userTest.js
@@ -15,8 +15,26 @@ describe('userCtrl', function()
   //testen her
   it('vi tester om det er det samme SSN nummer', function()
   {
-    var bruger = scope.getUserBySSN(1234561111);
-    expect(bruger.SSN == 1234561111);
+    var bruger = scope.getUserBySSN(1234567890);
+    expect(bruger.SSN == 1234567890);
+  })
+  
+  it('vi tester om det er det samme fornavn', function()
+  {
+    var bruger = scope.getUserByFirstname(Test);
+    expect(bruger.firstname == Test);
+  })
+  
+  it('vi tester om det er det samme efternavn', function()
+  {
+    var bruger = scope.getUserBySurname(Test);
+    expect(bruger.lastname == Test);
+  })
+  
+  it('vi tester om det er det samme e-mail', function()
+  {
+    var bruger = scope.getUserByEmail(Test@Eksempel.dk);
+    expect(bruger.email == Test@Eksempel.dk);
   })
 }
 })
